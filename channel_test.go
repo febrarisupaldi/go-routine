529ce42656024aae9bf78f318d5c0010fc3e3cb3
Febrari Supaldi <febrarisupaldi@outlook.com>
2022-04-06 16:09:12 +0700

    range channel

---
+++ channel_test.go
@@ -0,0 +1,94 @@
+package go_routine
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Eko Kurniawan Khannedy"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Eko Kurniawan Khannedy"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Eko Kurniawan Khannedy"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 2)
+	defer close(channel)
+
+	go func() {
+		channel <- "Eko"
+		channel <- "Kurniawan"
+	}()
+
+	func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+	go func() {
+		for i := 0; i < 100; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
+}
